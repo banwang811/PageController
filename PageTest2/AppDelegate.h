e3473e448f3d31938d9e9fa069f0295081a275f5
apple <apple@appletekiiMac-2.local>
2013-09-13 09:05:35 +0800

    Initial Commit

---
+++ PageTest2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PageTest2
+//
+//  Created by apple on 13-9-13.
+//  Copyright (c) 2013年 kongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
